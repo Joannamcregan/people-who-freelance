31044d12239d2b52d82578389569d2e67a405358
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-04-15 16:34:55 -0400

    Add files via upload

--- functions.php
+++ functions.php
@@ -2,6 +2,10 @@
 function pwf_files(){
     wp_enqueue_script('pwf-main-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('main-styles', get_stylesheet_directory_uri() . '/css/main.css', false, '', 'all');
+    wp_localize_script('pwf-main-js', 'pwfData', array(
+        'root_url' => get_site_url(),        
+        'nonce' => wp_create_nonce('wp_rest')
+    ));
 }
 
 add_action('wp_enqueue_scripts','pwf_files');
