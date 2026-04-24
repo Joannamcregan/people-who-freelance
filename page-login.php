03bf10f2d0dec6ebf2b17aa27ed3fe0c086b9e3b
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-04-24 16:44:18 -0400

    Add files via upload

--- page-login.php
+++ page-login.php
@@ -3,7 +3,7 @@
     <div class="max-width-700 centered-x margin-y-60">
         <h1>Come On In</h1>
         <h2 class="subheading">One day this will be a login page</h2>
-        <p>Freelancers and Clients who are member-owners will be able to enter their username and password for full site access. If you'd like to help bring the cooperative to life, please fill out the interest form below!</p>
+        <p>Freelancers and Clients will be able to enter their username and password for full site access. If you'd like to help bring the cooperative to life, please fill out the interest form below!</p>
         <?php echo do_shortcode('[forminator_form id="52"]'); ?>
         <?php get_template_part( 'template-parts/about'); ?>
     </div>
