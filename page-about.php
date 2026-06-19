0f643f2133c7adc26b6df12e726c16d2c9d2b5bd
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-19 14:07:04 -0400

    Add files via upload

--- page-about.php
+++ page-about.php
@@ -2,7 +2,7 @@
 ?><main>
     <div class="max-width-700 centered-x margin-y-60">
         <h1>A Cooperatively Owned Meeting Place for Freelancers and Clients</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tellus massa, pretium ac hendrerit non, laoreet quis ante. Sed erat ligula, convallis eu sodales quis, aliquam in orci. Integer non tempor turpis. Etiam consequat, erat nec pellentesque porttitor, nisi odio venenatis orci, in posuere eros ante vitae arcu. </p>
+        <p>One day this page will explain what our cooperatively owned networking platform for freelancers and clients <em>is</em>. For now, this demo site shows an example of <em>what it could be</em>. But why is a cooperatively owned networking platform for freelancers and clients needed, anyway?</p>
         <?php get_template_part( 'template-parts/about'); ?>
         <h2 class="subheading">Get Involved</h2>
         <p>Bringing this platform to life won't be easy, but it will be worth it. Want to help? Fill out the interest form below!</p>
