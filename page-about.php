cd9d6681f892702e714d190fc8dcb63e8760f9ba
Joannamcregan <58149780+Joannamcregan@users.noreply.github.com>
2026-06-19 13:51:07 -0400

    Add files via upload

--- page-about.php
+++ page-about.php
@@ -5,8 +5,9 @@
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tellus massa, pretium ac hendrerit non, laoreet quis ante. Sed erat ligula, convallis eu sodales quis, aliquam in orci. Integer non tempor turpis. Etiam consequat, erat nec pellentesque porttitor, nisi odio venenatis orci, in posuere eros ante vitae arcu. </p>
         <?php get_template_part( 'template-parts/about'); ?>
         <h2 class="subheading">Get Involved</h2>
-        <p>Want to help bring this platform to life? Fill out the interest form below!</p>
-        <?php echo do_shortcode('[forminator_form id="52"]'); ?>
+        <p>Bringing this platform to life won't be easy, but it will be worth it. Want to help? Fill out the interest form below!</p>
+        <?php echo do_shortcode('[forminator_form id="45"]'); ?>
+        <!-- 52 dev, 45 prod -->
     </div>
 </main>
 <?php get_footer(); ?>
--- page-join.php
+++ page-join.php
@@ -3,8 +3,9 @@
     <div class="max-width-700 centered-x margin-y-60">
         <h1>Join Us</h1>
         <h2 class="subheading">One day this will be a registration page</h2>
-        <p>Freelancers and Clients will be able to register as member-owners. This will give them full site access and make them co-owners of the cooperative, meaning they'll be able to help run the platform democratically. If you'd like to help bring the cooperative to life, please fill out the interest form below!</p>
-        <?php echo do_shortcode('[forminator_form id="52"]'); ?>
+        <p>Freelancers and Clients will be able to register as member-owners. This will give them full site access and make them co-owners of the cooperative, meaning they'll be able to participate in running the platform democratically. If you'd like to help bring the cooperative to life, please fill out the interest form below!</p>
+        <?php echo do_shortcode('[forminator_form id="45"]'); ?>
+        <!-- 52 dev, 45 prod -->
         <?php get_template_part( 'template-parts/about'); ?>
     </div>
 </main>
